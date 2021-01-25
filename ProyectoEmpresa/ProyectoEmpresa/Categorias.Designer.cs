fa7104cde9f4d5e94534a5d7589b99a3059b7cfb
Desarrollador Angel <ingangel89_velazco@hotmail.com>
2021-01-25 12:49:37 -0600

    Modificación Categorias

--- ProyectoEmpresa/ProyectoEmpresa/Categorias.Designer.cs
+++ ProyectoEmpresa/ProyectoEmpresa/Categorias.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 126);
+            this.label1.Location = new System.Drawing.Point(199, 231);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario: Pedro";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Categorias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 338);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Categorias";
             this.Text = "Categorias";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
